e738da6c6aa25434e2af5440f79fba355fde3329
Yanes666 <monweci@gmail.com>
2026-06-16 18:22:20 +0000

    Update config.js

--- app/js/config.js
+++ app/js/config.js
@@ -6,5 +6,5 @@
    3. Copiez "Project URL" et "anon public key" ci-dessous
    ============================================================ */
 
-const SUPABASE_URL      = "https://VOTRE-ID.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.VOTRE-CLE-ANON";
+const SUPABASE_URL      = https://ysaxtlvogxktuhqwxuvi.supabase.co;
+const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzYXh0bHZvZ3hrdHVocXd4dXZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2Mjg5NzQsImV4cCI6MjA5NzIwNDk3NH0.2gT5xyxWhJHlhQAx6jzTS7DMzqhbs-FMnHIwXNqA2ZM;
